2e9eea3fceca3f339d97c9111ed74288e8b9322e
kendermag <61602134+kendermag@users.noreply.github.com>
2021-11-21 22:39:19 +0100

    Update cameracapture_canny_rtmpstream.py
    Removing unused function and cv2 imshow

--- cameracapture_canny_rtmpstream.py
+++ cameracapture_canny_rtmpstream.py
@@ -6,8 +6,6 @@ from multiprocessing import Process
 
 ## TBA TODO: 
 
-## receive part is unused, to be deleted, also no need for creating cameracapture window on jetson
-
 ## removing UI from jetson would greatly enchant inference speed at higher resolutions, while decresaing battery usage
 
 def send():
@@ -43,32 +41,11 @@ def send():
         stream_frame = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
         out_send.write(stream_frame)
 
-        cv2.imshow('send', frame)
         if cv2.waitKey(1)&0xFF == ord('q'):
             break
 
     cap_send.release()
     out_send.release()
-
-def receive():
-    cap_receive = cv2.VideoCapture('udpsrc port=5000 caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! decodebin ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
-
-    if not cap_receive.isOpened():
-        print('VideoCapture not opened')
-        exit(0)
-
-    while True:
-        ret,frame = cap_receive.read()
-
-        if not ret:
-            print('empty frame')
-            break
-
-        cv2.imshow('receive', frame)
-        if cv2.waitKey(1)&0xFF == ord('q'):
-            break
-
-    cap_receive.release()
 
 if __name__ == '__main__':
     send()
